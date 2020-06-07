2ef8076bbecf1559f7d004bae97c94c226ee4571
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-06-07 23:01:53 +0300

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-
 #pragma comment(lib, "ws2_32.lib")
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -16,22 +15,33 @@
 #define IPADDR "127.0.0.1"
 #define PORT 9000
 #define NICK_LEN 20
+#define MAX_CLIENT 5
 
 void* do_send_chat(void* arg);
 
 void* do_receive_chat(void* arg);
 
 void str(char* arr, int length);
+char** read_data(char* file_name, int line_num);
+int words_num_in_file(char* file_name);
+
 
 pthread_t thread_1, thread_2;
-char first_mess[] = "Hello, dear Hacker! Who do you want to be today?\n1/Im bot.\n2.I aM nOt A bOt TrUsT mE";
-char write_name[] = "Input name: ";
+char first_mess[] = "Hello, dear Hacker! Who do you want to be today?\n1.Im bot.\n2.I aM nOt A bOt TrUsT mE\n";
+char    greeting[] = "=== WELCOME TO MY LIITLE SERVER ===\n";
+char write_name[] = "Input name:";
+char write_pass[] = "Write password:";
 char dialog[CHATDATA] = "===Dialog with ";
 char list[] = "===Here list of our clients:===\n";
 char notification[] = " send you a message";
 char update[] = "reload";
 char    escape[] = "exit";
+char welcome[] = "Access is allowed\n";
+char start_mess[] = "View dialog with ->";
+
 char    nickname[NICK_LEN];
+char password[NICK_LEN];
+
 int flag = 0;
 
 int main(int argc, char* argv[])
@@ -58,27 +68,58 @@ int main(int argc, char* argv[])
         printf("Can not connect\n");
         return -1;
     }
-    memset(chatData, 0, sizeof(chatData));
-
+    memset(chatData, 0, sizeof(NICK_LEN));
+    memset(nickname, 0, sizeof(NICK_LEN));
+    memset(password, 0, sizeof(NICK_LEN));
     //здесь нужно вставить вариант с ботом
     //
     //
     //
     //
+    char file_name[] = "bot_names.txt";
+    char passwords[] = "bot_passwords.txt";
 
-    printf(first_mess);
-    fgets(nickname, NICK_LEN, stdin);
+    printf(greeting);// welcome mess
+
+    printf(first_mess);// choose mod
+    fgets(chatData, CHATDATA, stdin);
     fflush(stdin);
-    str(nickname, NICK_LEN);
+    str(chatData, CHATDATA);
     //scanf("%s", nickname);
 
-    send(c_socket, nickname, strlen(nickname),0);
+    if (atoi(chatData) == 1) {
+        srand(time(NULL));
+        int word_line = words_num_in_file(file_name);
+        char** data = read_data(file_name, word_line);
+        int random = rand() % word_line;
+        strcpy(nickname,data[random] );
+        word_line = words_num_in_file(passwords);
+        data = read_data(passwords, word_line);
+        random = rand() % word_line;
+        strcpy(password, data[random]);
+        flag = 1;
+    }
+    else {
+        printf(write_name);
+        fgets(nickname, NICK_LEN, stdin);
+        fflush(stdin);
+        printf(write_pass);
+        fgets(password, NICK_LEN, stdin);
+        fflush(stdin);
+    }
+
+    send(c_socket, nickname, strlen(nickname), 0);//send mess and password
+    send(c_socket, password, strlen(password), 0);
 
     memset(chatData, 0, sizeof(chatData));
     recv(c_socket, chatData, sizeof(chatData), 0);
-
-    printf("%s", chatData);// print welcome mess
-
+    while (!strstr(welcome, chatData)) {
+        memset(password, 0, sizeof(NICK_LEN));
+        printf(write_pass);
+        fgets(password, NICK_LEN, stdin);
+        fflush(stdin);
+    }
+   
     pthread_create(&thread_2, NULL, do_receive_chat, (void*)&c_socket);
     pthread_create(&thread_1, NULL, do_send_chat, (void*)&c_socket);
 
@@ -109,10 +150,12 @@ void * do_send_chat (void* arg){
 	}
    flag = 1;
 }
+
 void* do_receive_chat(void* arg)
 {
+    char bot_buf[MAX_CLIENT][MAX_CLIENT] ;
     char    chatData[CHATDATA];
-    int    n;
+    int    n = 0, pointer = 0, flag = 0;
     SOCKET   c_socket = *(SOCKET*)arg;        // client socket
     while (1) {
         memset(chatData, 0, sizeof(chatData));
@@ -120,10 +163,23 @@ void* do_receive_chat(void* arg)
             if (strstr(chatData, dialog) != NULL || strstr(chatData,list)) {// client want to update screen
                 system("cls");
             }
-            printf("%s", chatData);
+            if (strstr(chatData, list)) {
+                pointer = 0;
+                flag = 1;
+            }
             //hourse head
-            if (strstr(chatData, notification)) {
+            printf("%s", chatData);
+            if (flag) {
+                strcpy(bot_buf[pointer++], chatData);
+            }
+            if (strstr(chatData, start_mess)) {
+                srand(time(NULL));
+                int random = rand() % pointer;
+                send(c_socket, bot_buf[random], sizeof(bot_buf[random]), 0);
                 Sleep(3000);
+            }
+            if (strstr(chatData, notification)) {
+                Sleep(4000);
                 for (int i = 0; i < strlen(chatData); i++) {//clean notification
                     printf("\b \b");
                 }
@@ -132,6 +188,7 @@ void* do_receive_chat(void* arg)
         }
     }
 }
+
 void str (char* arr, int length) {
 	int i;
 	for (i = 0; i < length; i++) { // trim \n
@@ -140,4 +197,44 @@ void str (char* arr, int length) {
 			break;
 		}
 	}
+}
+
+char** read_data(char* file_name, int line_num) {
+    char** data = (char**)calloc(line_num, sizeof(char*));
+    for (int i = 0; i < line_num; i++) {
+        data[i] = (char*)calloc((NICK_LEN), sizeof(char));
+    }
+    FILE* file = fopen(file_name, "r");
+    if (file == NULL) {
+        exit (-7);
+    }
+    int pointer = 0;
+    while (!feof(file)) {
+        fgets(data[pointer], 100, file);
+        data[pointer][strlen(data[pointer]) - 1] = ' ';
+        pointer++;
+    }
+    fclose(file);
+    return data;
+}
+
+int words_num_in_file(char* file_name) {
+
+    int line_num = 0, any;
+    FILE* file = fopen(file_name, "r");
+    if (file == NULL) {
+        return -7;
+    }
+    do {
+        any = fgetc(file);
+        if (any == '\n') {
+            line_num++;
+        }
+    } while (any != EOF);
+
+    line_num++;
+
+    fclose(file);
+
+    return line_num;
 }
