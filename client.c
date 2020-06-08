148361d4ef280c2189c70c0aaed62562e982c491
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-06-08 17:12:09 +0300

    Update client.c

--- client.c
+++ client.c
@@ -1,314 +1,315 @@
-//#include "pch.h"
+
+#pragma comment(lib, "ws2_32.lib")
+
 #define _CRT_SECURE_NO_WARNINGS
 #define HAVE_STRUCT_TIMESPEC
-#include <pthread.h>
-#pragma comment(lib, "ws2_32.lib")
 #include <winsock.h>
 #include <stdio.h>
-#include<string.h>
 #include <stdlib.h>
-#include<time.h>
+#include<pthread.h>
+#include <signal.h>
+#include<string.h>
+#include <windows.h>
 
-void* do_chat(void* arg);
-
-int pushClient(int roomNum, SOCKET c_socket, char* nickname);
-
-void* print_cleints(SOCKET soket_client);
-
-int* find_mate_box(char* name);
-
-void str(char* arr, int length);
-
-void* print_history(int room_mate, int room_client);
-
-pthread_t thread;
-pthread_mutex_t mutex;
-
-#define MAX_LEN 20
-#define MAX_CLIENT 5
+#define LENGTH 2048
 #define CHATDATA 1024
-#define INVALID_SOCK -1
+#define IPADDR "127.0.0.1"
 #define PORT 9000
+#define NICK_LEN 20
+#define MAX_CLIENT 5
 
-typedef struct guest {
-	char nickname[MAX_LEN];
-	char password[MAX_LEN];
-	char history[CHATDATA][CHATDATA];
-	int last_mess;
-	SOCKET socket;
-} guest;
+void* do_send_chat(void* arg);
 
-typedef struct socket_info {
-	int roomNum;
-	SOCKET socket;
-} socket_info;
+void* do_receive_chat(void* arg);
+char** read_data(char* file_name, int line_num);
+void str(char* arr, int length);
+int words_num_in_file(char* file_name);
+void* chat_bot(void* arg);
 
-int cli_count = 0;
-
-guest list_c[MAX_CLIENT][MAX_CLIENT];
-
-char dialog[CHATDATA] = "===Dialog with ";
-char    escape[] = "exit";
-char    greeting[] = "=== WELCOME TO THE CHATROOM ===\n";
-char start_mess[] = "View dialog with ->";
-char    CODE200[] = "Sorry No More Connection\n";
-char	neUser[] = "This is Not User\n";
-char	denied[] = "Sorry, assess denied\n";
-char update[] = "reload";
-char clear_history[] = "Sorry, no previous messages. Start chattering right now\n";
-char notification[] = " send you a message\n";
+pthread_t thread_1, thread_2;
+char first_mess[] = "Hello, dear Hacker! Who do you want to be today?\n1/Im bot.\n2.I aM nOt A bOt TrUsT mE\n";
 char one_client[] = "===Your are alone, don't worry, please,wait===\n";
-char list[] = "===Here list of our clients:===\n";
-char end[] = "--------------------------------------------\n";
-
+char write_name[] = "Input name: ";
+char dialog[CHATDATA] = "===Dialog with ";
+char list[] = "Here list of our clients";
+char    greeting[] = "=== WELCOME TO MY LIITLE SERVER ===\n";
+char notification[] = " send you a message";
+char write_pass[] = "Write password:";
+char update[] = "reload";
+char    escape[] = "exit";
+char start_mess[] = "View dialog with ->";
+char end[] = "--------------------------------------------";
+const char    nickname[NICK_LEN];
+char password[NICK_LEN];
+int flag_bot = 0;
 
 int main(int argc, char* argv[])
 {
-	WSADATA wsd;
-	if (WSAStartup(MAKEWORD(1, 1), &wsd) == 0)
-	{
-		printf("Connected to socket lib\n");
-	}
-	SOCKET c_socket, s_socket;
-	socket_info si;
-	struct sockaddr_in localaddr, clientaddr;
-	int    len;
-	int    i, j, n;
-	int    res;
-	char nickname[20];
-	char password[MAX_LEN];
-	char roomNum[3]; int r_Num;
-	if (pthread_mutex_init(&mutex, NULL) != 0) {
-		printf("Can not create mutex\n");
-		return -1;
-	}
-	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+    WSADATA wsd;
+    if (WSAStartup(MAKEWORD(1, 1), &wsd) != 0)
+    {
+        printf("Can't connect to socket lib");
+        return 1;
+    }
+    SOCKET c_socket;
+    struct sockaddr_in c_addr;
+    int len;
+    char chatData[CHATDATA];
+    char buf[CHATDATA];
+    int nfds;
+    fd_set read_fds;
+    int n;
+    c_socket = socket(PF_INET, SOCK_STREAM, 0);
+    c_addr.sin_addr.S_un.S_addr = inet_addr(IPADDR);
+    c_addr.sin_family = AF_INET;
+    c_addr.sin_port = htons(PORT);
+    if (connect(c_socket, (struct sockaddr*) & c_addr, sizeof(c_addr)) == -1) {
+        printf("Can not connect\n");
+        return -1;
+    }
+ 
 
-	localaddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-	localaddr.sin_family = AF_INET;
-	localaddr.sin_port = htons(PORT);//port number is for example, must be more than 1024
-	;
-	if (bind(s_socket, (struct sockaddr*) & localaddr, sizeof(localaddr)) == SOCKET_ERROR) {
-		printf("Can not Bind\n");
-		return -1;
-	}
-	if (listen(s_socket, MAX_CLIENT) == -1) {
-		printf("listen Fail\n");
-		return -1;
-	}
-	for (i = 0; i < MAX_CLIENT; i++) {//CLEAN ROOMS 
-		for (j = 0; j < MAX_CLIENT; j++) {
-			list_c[i][j].last_mess = 0;
-			list_c[i][j].socket = INVALID_SOCK;
-			strcpy(list_c[i][j].nickname, "\0");
-		}
-	}
-	while (1) {
+    //здесь нужно вставить вариант с ботом
+    //
+    //
+    //
+    //
 
-		len = sizeof(clientaddr);
-		c_socket = accept(s_socket, (struct sockaddr*) & clientaddr, &len);
-		cli_count++;
+    
 
-		memset(nickname, 0, sizeof(nickname));//clean func
-		recv(c_socket, nickname, sizeof(nickname), 0);// write name
+    memset(chatData, 0, sizeof(chatData));
+    memset(nickname, 0, sizeof(nickname));
+    memset(password, 0, sizeof(password));
+    //здесь нужно вставить вариант с ботом
+    //
+    //
+    //
+    //
+    char file_name[] = "bot_names.txt";
+    char passwords[] = "bot_passwords.txt";
 
-		memset(password, 0, sizeof(password));
-		recv(c_socket, password, sizeof(password), 0);
-		//printf("%s %s\n", nickname, password);
+    printf(greeting);// welcome mess
 
-		int res = pushClient(cli_count - 1, c_socket, nickname);// push socket into room box
-		if (res < 0) { //
-			send(c_socket, denied, strlen(CODE200), 0);
-			closesocket(c_socket);
-		}
-		else {
-			printf(" name = %s, passowrd = %s, cli_cpount = %d\n", nickname, password, cli_count);
-			send(c_socket, greeting, sizeof(greeting), 0);//welcome mess
-			Sleep(100);
-			pthread_create(&thread, NULL, do_chat, (void*)(cli_count - 1));
-		}
-	}
+    printf(first_mess);
+  
+    fgets(chatData, CHATDATA, stdin);
+    fflush(stdin);
+    
+    str(chatData, CHATDATA);
+   
+    if (atoi(chatData) == 1) {
+        srand(time(NULL));
+        int word_line = words_num_in_file(file_name);
+        char** data = read_data(file_name, word_line);
+        int random = rand() % (word_line - 1);
+        strcpy(nickname, data[random]);
+        word_line = words_num_in_file(passwords);
+        data = read_data(passwords, word_line);
+        random = rand() % (word_line - 1);
+        strcpy(password, data[random]);
+        flag_bot = 1;
+    }
+    else {
+        printf(write_name);
+        fgets(nickname, NICK_LEN, stdin);
+        fflush(stdin);
+        printf(write_pass);
+        fgets(password, NICK_LEN, stdin);
+        fflush(stdin);
+    }
+    //printf("%s %s", password, nickname);
+    str(nickname, strlen(nickname));
+    str(password, strlen(password));
+
+    send(c_socket, nickname, strlen(nickname), 0);//send mess and password
+    send(c_socket, password, strlen(password), 0);
+    if (flag_bot) {
+        pthread_create(&thread_2, NULL, chat_bot, (void*)&c_socket);
+    }
+    else {
+        pthread_create(&thread_2, NULL, do_receive_chat, (void*)&c_socket);
+        pthread_create(&thread_1, NULL, do_send_chat, (void*)&c_socket);
+    }
+
+    pthread_join(thread_1, (void**)NULL);
+    pthread_join(thread_2, (void**)NULL);
+    closesocket(c_socket);
 }
 
-void* print_cleints(SOCKET soket_client) {
-	char mess[CHATDATA];
-	for (int i = 0; i < MAX_CLIENT; i++) {
-		if (soket_client != list_c[i][0].socket && list_c[i][0].socket != INVALID_SOCK) {//print all except client
-			strcpy(mess, list_c[i][0].nickname);
-			strcat(mess, "\n");
-			send(soket_client, mess, sizeof(mess), 0);
-		}
-	}
+void* do_send_chat(void* arg) {
+    char chatData[CHATDATA];
+    char buf[CHATDATA];
+    //int n;
+    SOCKET c_socket = *(SOCKET*)arg;
+    while (1) {
+
+        memset(chatData, '\0', sizeof(chatData));
+        memset(buf, '\0', sizeof(buf));
+        
+        fgets(buf, CHATDATA, stdin);
+        
+        fflush(stdin);
+        //Sleep(100);
+        //scanf("%s", buf);
+
+        str(buf, CHATDATA);
+ 
+        sprintf(chatData, "%s: %s", nickname, buf);
+        send(c_socket, chatData, strlen(chatData), 0);
+
+    }
+    //flag = 1;
 }
-
-
-int* find_mate_box(char* name) {
-	//return i if nick_name exist 
-	//return -1 if no match 
-	char* istr;
-	istr = strtok(name, ":");
-	istr = strtok(NULL, "\0");
-	for (int i = 0; i < MAX_CLIENT; i++) {
-		if (strstr(istr, list_c[i][0].nickname)) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-void* print_history(int room_mate, int room_client) {
-
-	int lst_mess = list_c[room_client][room_mate].last_mess;
-	if (lst_mess == 0) {
-		send(list_c[room_client][0].socket, clear_history, sizeof(clear_history), 0);// no history print mess
-	}
-	for (int i = 0; i < lst_mess; i++) {
-		send(list_c[room_client][0].socket, list_c[room_client][room_mate].history[i], sizeof(list_c[room_client][room_mate].history[i]), 0);
-	}
-}
-
-void str(char* arr, int length) {
-	int i;
-	for (i = 0; i < length; i++) { // trim \n
-		if (arr[i] == '\n') {
-			arr[i] = '\0';
-			break;
-		}
-	}
-}
-
-void* do_chat(void* arg)
+void* do_receive_chat(void* arg)
 {
-	int room = (int)arg;
-
-	SOCKET c_socket = list_c[room][0].socket;
-
-	char mate_name[MAX_LEN];
-	char chatData[CHATDATA];
-	char buffer[CHATDATA];
-
-	int i = 0, j = 0, n = 0;
-	while (1) {
-		if (cli_count != 1) {
-			memset(mate_name, 0, sizeof(mate_name));
-			pthread_mutex_lock(&mutex);
-	
-			send(c_socket, list, sizeof(list), 0);
-			pthread_mutex_unlock(&mutex);
-
-			Sleep(100);//we need pause to clean console
-			pthread_mutex_lock(&mutex);
-			print_cleints(c_socket);//print all clients except user
-			pthread_mutex_unlock(&mutex);
-			pthread_mutex_lock(&mutex);
-			send(c_socket, start_mess, sizeof(start_mess), 0);//send "view dialog with "
-			pthread_mutex_unlock(&mutex);
-			recv(c_socket, mate_name, sizeof(mate_name), 0);//recv name
-			if (strstr(mate_name, escape) == NULL) {// if user do not want to leave
-
-				int mate_num_box = find_mate_box(mate_name);// res = number of mate_box
-
-				if (mate_num_box >= 0) {//if we found mate
-					i = pushClient(room, c_socket, list_c[mate_num_box][0].nickname);
-					j = pushClient(mate_num_box, c_socket, list_c[room][0].nickname);
-
-					if ((i != -1) && (j != -1)) {//if succesfully push both clients
-						//start chat
-						memset(chatData, 0, sizeof(chatData));
-
-						while (1) {
-							//print history
-							memset(buffer, 0, sizeof(buffer));//dialog icon
-							strcpy(buffer, dialog);
-							strcat(buffer, list_c[mate_num_box][0].nickname);
-							strcat(buffer, "===\n");
-
-							pthread_mutex_lock(&mutex);
-							send(c_socket, buffer, sizeof(buffer), 0);
-							pthread_mutex_unlock(&mutex);
-							Sleep(100);//because we need time to clean console 
-							memset(chatData, 0, sizeof(chatData));
-
-							pthread_mutex_lock(&mutex);
-							print_history(i, room);//print previous mess
-							pthread_mutex_unlock(&mutex);
-
-							send(c_socket, end, sizeof(end), 0);
-
-							pthread_mutex_lock(&mutex);
-							n = recv(c_socket, chatData, sizeof(chatData), 0);
-							pthread_mutex_unlock(&mutex);
-							//3 options
-
-							if (strstr(chatData, escape)) {//if user want to leave
-								break;
-							}
-							else if (strstr(chatData, update)) {
-								continue;
-							}
-							else {// if user send a mess
-								struct tm* u;//define time
-								const time_t timer = time(NULL);
-								u = localtime(&timer);
-								char s1[CHATDATA] = { 0 };
-								strftime(s1, CHATDATA, "[ %H : %M ] ", u);
-								strcat(s1, chatData);
-								strcat(s1, "\n");
-
-								strcpy(list_c[room][i].history[list_c[room][i].last_mess], s1);//save mess in user history
-								list_c[room][i].last_mess++;
-								strcpy(list_c[mate_num_box][j].history[list_c[mate_num_box][j].last_mess], s1);//save mess in mate history
-								list_c[mate_num_box][j].last_mess++;
-								memset(chatData, 0, sizeof(chatData));
-								strcpy(chatData, list_c[room][0].nickname);
-								strcat(chatData, notification);
-								pthread_mutex_lock(&mutex);
-								send(list_c[mate_num_box][0].socket, chatData, sizeof(chatData), 0);//send notification about new mess to mate
-								pthread_mutex_unlock(&mutex);
-
-							}
-						}
-
-					}
-				}
-				else {
-					send(c_socket, neUser, sizeof(neUser), 0);//wrong name
-				}
-			}
-			else {
-				send(c_socket, escape, sizeof(escape), 0);
-				closesocket(c_socket);
-				break;
-			}
-		}
-		else {
-			send(c_socket, one_client, sizeof(one_client), 0);//please wait mess
-			Sleep(10000);
-		}
-	}
+    char    chatData[CHATDATA];
+    int    n;
+    SOCKET   c_socket = *(SOCKET*)arg;        // client socket
+    while (1) {
+        memset(chatData, 0, sizeof(chatData));
+        if ((n = recv(c_socket, chatData, sizeof(chatData), 0)) > 0) {
+            if (strstr(chatData, dialog) != NULL || strstr(chatData, list)) {// client want to update screen
+                system("cls");
+            }
+            printf("%s", chatData);
+            //hourse head
+            if (strstr(chatData, notification)) {
+                Sleep(3000);
+                for (int i = 0; i < strlen(chatData); i++) {//clean notification
+                    printf("\b \b");
+                }
+                send(c_socket, update, sizeof(update), 0);
+            }
+        }
+    }
+}
+void str(char* arr, int length) {
+    int i;
+    for (i = 0; i < length; i++) { // trim \n
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
+    }
+}
+char** read_data(char* file_name, int line_num) {
+    char** data = (char**)calloc(line_num, sizeof(char*));
+    for (int i = 0; i < line_num; i++) {
+        data[i] = (char*)calloc((NICK_LEN), sizeof(char));
+    }
+    FILE* file = fopen(file_name, "r");
+    if (file == NULL) {
+        exit(-7);
+    }
+    int pointer = 0;
+    while (!feof(file)) {
+        fgets(data[pointer], 100, file);
+        data[pointer][strlen(data[pointer]) - 1] = '\0';
+        pointer++;
+    }
+    fclose(file);
+    return data;
 }
 
-int pushClient(int roomNum, SOCKET c_socket, char* nickname) {
-	//ADD c_socket to list_c array.
-	//
-	///////////////////////////////
-	//return -1, if too much clients
-	//return the index of list_c which c_socket is added.
-	int i;
-	for (i = 0; i < MAX_CLIENT; i++) {//puch into free room
-		if (list_c[roomNum][i].socket == INVALID_SOCK || (strstr(list_c[roomNum][i].nickname, nickname))) {
-			//if dialog exist or we found free place
-			list_c[roomNum][i].socket = c_socket;
-			strcpy(list_c[roomNum][i].nickname, nickname);
-			break;
-		}
-	}
-	if (i == MAX_CLIENT || cli_count == MAX_CLIENT) {
-		i = -1;
-	}
-	return i; // list_c index.
+int words_num_in_file(char* file_name) {
+
+    int line_num = 0, any;
+    FILE* file = fopen(file_name, "r");
+    if (file == NULL) {
+        return -7;
+    }
+    do {
+        any = fgetc(file);
+        if (any == '\n') {
+            line_num++;
+        }
+    } while (any != EOF);
+
+    line_num++;
+
+    fclose(file);
+
+    return line_num;
 }
 
+void* chat_bot(void* arg)
+{
 
+    char bot_buf[MAX_CLIENT][CHATDATA];
+    char chatData[CHATDATA];
+    int n = 0, pointer = 0, flag = 0;
+    char buf[CHATDATA];
+    char file_name[] = "bot_phrase.txt";
+    int word_line = words_num_in_file(file_name);
+    char** data = read_data(file_name, word_line);
+    int random = 0;
+    srand(time(0));
+    SOCKET c_socket = *(SOCKET*)arg;        // bot socket
+    while (1) {
+        memset(chatData, 0, sizeof(chatData));
+        
+        if ((n = recv(c_socket, chatData, sizeof(chatData), 0)) > 0) {
+ 
+            //printf("1 = %s\n", chatData);
+            if (strstr(chatData, end) != NULL) {
+                printf("%s", chatData);
+                pointer = 0;
+                flag = 0;
+                memset(chatData, '\0', sizeof(chatData));
+                memset(buf, '\0', sizeof(buf));
+
+                sprintf(buf, data[rand() % word_line]);
+
+                str(buf, CHATDATA);
+                Sleep(4000);
+                sprintf(chatData, "%s: %s", nickname, buf);
+                send(c_socket, chatData, strlen(chatData), 0);
+                Sleep(1000);
+                send(c_socket, escape, strlen(escape), 0);
+            }
+            else {
+                //Sleep(2000);
+                if (strstr(chatData, list) || strstr(chatData, dialog)) {// client want to update screen
+                    printf("\ncleen\n");
+                    flag = 0;
+                    //system("cls");
+                    Sleep(100);
+                }
+                printf("%s", chatData);
+                if (flag && !strstr(chatData, start_mess) && !strstr(chatData,one_client )) {
+                    str(chatData, strlen(chatData));
+                   // printf("ok %s",chatData);
+                    strcpy(bot_buf[pointer++], chatData);
+                }
+                if (strstr(chatData, list)) {
+                    pointer = 0;
+                    flag = 1;
+                }
+                //printPony();
+                
+                
+                if (strstr(chatData, notification) && flag_bot == 0) {
+                    Sleep(4000);
+                    for (int i = 0; i < strlen(chatData); i++) { //clean notification
+                        printf("\b \b");
+                    }
+                    send(c_socket, update, sizeof(update), 0);
+                }
+
+                if (strstr(chatData, start_mess)) { //delete aaa
+                    //printf("%s",bot_buf[0]);
+                    memset(chatData, '\0', sizeof(chatData));
+                    random = rand() % (pointer);
+                    //printf(" p= %d %d", pointer,random);
+                    Sleep(3000);
+                    sprintf(chatData, "%s: %s", nickname, bot_buf[random]);
+                    printf("%s", bot_buf[random]);
+                    send(c_socket, chatData, sizeof(chatData), 0);
+                }
+                
+            }
+        }
+    }
+}
 
 
